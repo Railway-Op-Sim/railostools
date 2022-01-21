6bd5d38369305181539fdfa8700af50638d1bd68
jkwok678 <jkwok678@gmail.com>
2022-01-21 23:00:16 +0000

    Fix "Testing TOML Parsing" test.

--- java/src/test/java/io/github/ros/MetadataTest.java
+++ java/src/test/java/io/github/ros/MetadataTest.java
@@ -1,8 +1,5 @@
 package io.github.ros;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
 import java.io.File;
 import io.github.ros.MetadataBuilder.MetadataValidationError;
 import io.github.ros.MetadataBuilder;
@@ -10,12 +7,17 @@ import io.github.ros.MetadataBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MetadataTest {
 
     @Test
     @DisplayName("Testing TOML Parsing")
     public void tomlParse() {
-        MetadataBuilder mb = new MetadataBuilder(new File("src/test/java/io/github/ros/data/Antwerpen_Centraal.toml"));
+
+        MetadataBuilder mb = assertDoesNotThrow(() -> new MetadataBuilder(
+                new File("src/test/java/io/github/ros/data/Antwerpen_Centraal.toml")));
+
         assertEquals("Simulation of Antwerp south", mb.metadata.name);
         assertEquals("Krizar", mb.metadata.author);
         assertEquals(Boolean.TRUE, mb.metadata.factual);
