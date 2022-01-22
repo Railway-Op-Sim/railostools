3adac3029bdf749b57289e02dc4936551c522df0
Kristian Zarębski <krizar312@gmail.com>
2022-01-22 16:36:39 +0000

    Add Metadata write functionality (#8)
    * Add release date to C++
    
    * Add toml write capability to C++

--- cpp/include/rostools/metadata.hxx
+++ cpp/include/rostools/metadata.hxx
@@ -6,6 +6,7 @@
 #include <vector>
 #include <sstream>
 #include <filesystem>
+#include <fstream>
 
 #include "tomlplusplus/toml.hpp"
 #include "semver/semver.hpp"
@@ -23,6 +24,8 @@ namespace ROSTools {
         "author",
         "country_code",
         "factual",
+        "version",
+        "release_date",
     };
 
     class Metadata {
@@ -31,9 +34,15 @@ namespace ROSTools {
             toml::table meta_data_;
             std::vector<std::string> retrieve_list_(const std::string& key) const;
             std::string retrieve_string_(const std::string& key) const;
+            template<typename T>
+            void set_key_value_(const std::string& key, const T& value);
+            void append_to_list_(const std::string& label, const std::string& key, const std::string& value);
+            void set_list_(const std::string& key, const std::vector<std::string>& list_vals);
+            void validate_();
         public:
             Metadata() {}
             Metadata(const std::filesystem::path& file_name);
+            toml::table data() const {return meta_data_;}
             std::string name() const;
             std::filesystem::path rly_file() const;
             semver::version version() const;
@@ -47,10 +56,36 @@ namespace ROSTools {
             std::vector<std::filesystem::path> graphic_files() const;
             std::vector<std::string> contributors() const;
             bool factual() const;
+            int difficulty() const;
             int year() const;
             std::string description() const;
             std::string author() const;
             date::year_month_day release_date() const;
+            void setRLYFile(const std::string& rly_filename);
+            void addTTBFile(const std::string& ttb_filename);
+            void setTTBFiles(const std::vector<std::string>& ttb_filenames);
+            void addSSNFile(const std::string& ssn_filename);
+            void setSSNFiles(const std::vector<std::string>& ssn_filenames);
+            void addImgFile(const std::string& img_filename);
+            void setImgFiles(const std::vector<std::string>& img_filenames);
+            void addGraphicFile(const std::string& graphic_filename);
+            void setGraphicFiles(const std::vector<std::string>& graphic_filenames);
+            void addDocFile(const std::string& doc_filename);
+            void setDocFiles(const std::vector<std::string>& doc_filenames);
+            void setAuthor(const std::string& author);
+            void addContributor(const std::string& author);
+            void setYear(const int year);
+            void setDifficulty(const int difficulty);
+            void setContributors(const std::vector<std::string>& contributor_list);
+            void setFactual(bool is_factual);
+            void setReleaseDate(const date::year_month_day& release_date_val);
+            void setDescription(const std::string& description_val);
+            void setDisplayName(const std::string& display_name_val);
+            void setName(const std::string& name_val);
+            void setCountryCode(const std::string& country_code_val);
+            void write(const std::filesystem::path& output_file);
+            void setVersion(const semver::version& version);
+            void setVersion(const std::string& version);
     };
 };
 
