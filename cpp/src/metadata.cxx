1f57ec6adb5e75a253daf265d56131dca03f4c47
Kristian Zarebski <krizar312@gmail.com>
2022-01-22 14:24:44 +0000

    Add toml write capability to C++

--- cpp/src/metadata.cxx
+++ cpp/src/metadata.cxx
@@ -4,6 +4,11 @@ std::string ROSTools::Metadata::retrieve_string_(const std::string& key) const {
     return meta_data_[key].value<std::string>().value_or("");
 }
 
+template<typename T>
+void ROSTools::Metadata::set_key_value_(const std::string& key, const T& value) {
+    meta_data_.insert_or_assign(key, toml::value{value});
+}
+
 std::string ROSTools::Metadata::name() const {
     return retrieve_string_("name");
 }
@@ -22,6 +27,10 @@ std::string ROSTools::Metadata::display_name() const {
 
 int ROSTools::Metadata::year() const {
     return meta_data_["year"].value<int>().value_or(-1);
+}
+
+int ROSTools::Metadata::difficulty() const {
+    return meta_data_["difficulty"].value<int>().value_or(-1);
 }
 
 bool ROSTools::Metadata::factual() const {
@@ -134,4 +143,147 @@ date::year_month_day ROSTools::Metadata::release_date() const {
     }
 
     return date::year{date_elements_[0]}/date::month{date_elements_[1]}/date::day{date_elements_[2]};
+}
+
+void ROSTools::Metadata::setAuthor(const std::string& author) {
+    if(author.empty()) {
+        throw std::runtime_error("Key 'author' cannot be empty");
+    }
+    set_key_value_("author", author);
+}
+
+void ROSTools::Metadata::setFactual(bool is_factual) {
+    set_key_value_("factual", is_factual);
+}
+
+void ROSTools::Metadata::setReleaseDate(const date::year_month_day& release_date_val) {
+    std::stringstream ss;
+    ss << release_date_val;
+    set_key_value_("release_date", ss.str());
+}
+
+void ROSTools::Metadata::set_list_(const std::string& key, const std::vector<std::string>& list_vals) {
+    toml::array out_vals_;
+
+    for(const std::string& val : list_vals) {
+      out_vals_.push_back(val);
+    }
+    meta_data_.insert_or_assign(key, out_vals_);
+}
+
+void ROSTools::Metadata::append_to_list_(const std::string& label, const std::string& key, const std::string& value) {
+    std::vector<std::string> existing_ = retrieve_list_(key);
+    if(std::find(existing_.begin(), existing_.end(), value) != existing_.end()) {
+        throw std::runtime_error(label+" '"+value+"' already in list.");
+    }
+    existing_.push_back(value);
+    set_list_(key, existing_);
+}
+
+void ROSTools::Metadata::setContributors(const std::vector<std::string>& contributor_list) {
+    set_list_("contributors", contributor_list);
+}
+
+void ROSTools::Metadata::setTTBFiles(const std::vector<std::string>& ttb_filenames) {
+    set_list_("ttb_files", ttb_filenames);
+}
+
+void ROSTools::Metadata::setSSNFiles(const std::vector<std::string>& ssn_filenames) {
+    set_list_("ssn_files", ssn_filenames);
+}
+
+void ROSTools::Metadata::setImgFiles(const std::vector<std::string>& img_filenames) {
+    set_list_("img_files", img_filenames);
+}
+
+void ROSTools::Metadata::setGraphicFiles(const std::vector<std::string>& graphic_filenames) {
+    set_list_("graphic_files", graphic_filenames);
+}
+
+void ROSTools::Metadata::setDocFiles(const std::vector<std::string>& doc_filenames) {
+    set_list_("doc_files", doc_filenames);
+}
+
+void ROSTools::Metadata::setRLYFile(const std::string& rly_filename){
+    if(rly_filename.empty()) {
+        throw std::runtime_error("Key 'rly_file' cannot be empty");
+    }
+    set_key_value_("rly_file", rly_filename);
+}
+
+void ROSTools::Metadata::setDisplayName(const std::string& display_name_val){
+    set_key_value_("display_name", display_name_val);
+}
+
+void ROSTools::Metadata::setName(const std::string& name_val){
+    if(name_val.empty()) {
+        throw std::runtime_error("Key 'name' cannot be empty");
+    }
+    set_key_value_("name", name_val);
+}
+
+void ROSTools::Metadata::setDescription(const std::string& description_val){
+    set_key_value_("description", description_val);
+}
+
+void ROSTools::Metadata::setYear(const int year){
+    if(year < 1900 || year > 9999) {
+        throw std::runtime_error("Year must be in range [1999,9999]");
+    }
+    set_key_value_("year", year);
+}
+
+void ROSTools::Metadata::setDifficulty(const int difficulty_val){
+    if(difficulty_val < 1 || difficulty_val > 5) {
+        throw std::runtime_error("Difficulty must be in the range [1, 5]");
+    }
+    set_key_value_("difficulty", difficulty_val);
+}
+
+void ROSTools::Metadata::addContributor(const std::string& contributor){
+    append_to_list_("Contributor", "contributors", contributor);
+}
+
+void ROSTools::Metadata::addTTBFile(const std::string& ttb_filename){
+    append_to_list_("Timetable file", "ttb_files", ttb_filename);
+}
+
+void ROSTools::Metadata::addSSNFile(const std::string& ssn_filename){
+    append_to_list_("Session file", "ssn_files", ssn_filename);
+}
+
+void ROSTools::Metadata::addImgFile(const std::string& img_filename){
+    append_to_list_("Image file", "img_files", img_filename);
+}
+
+void ROSTools::Metadata::addGraphicFile(const std::string& graphic_filename){
+    append_to_list_("Graphic file", "graphic_files", graphic_filename);
+}
+
+void ROSTools::Metadata::addDocFile(const std::string& doc_filename){
+    append_to_list_("Document file", "doc_files", doc_filename);
+}
+
+void ROSTools::Metadata::setCountryCode(const std::string& country_code_val) {
+    if(COUNTRY_CODES.count(country_code_val) == 0) {
+        throw std::runtime_error("Invalid country code '"+country_code_val+"'");
+    }
+    set_key_value_("country_code", country_code_val);
+}
+
+void ROSTools::Metadata::write(const std::filesystem::path& output_file) {
+    std::ofstream outs;
+    outs.open(output_file);
+
+    outs << meta_data_ << "\n";
+
+    outs.close();
+}
+
+void ROSTools::Metadata::setVersion(const semver::version& version) {
+    set_key_value_("version", semver::to_string(version));
+}
+
+void ROSTools::Metadata::setVersion(const std::string& version) {
+    setVersion(semver::version{version});
 }
