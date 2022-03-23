35d16bf927529f0d1e28ea6694836e06efc9d08f
Kristian Zarębski <krizar312@gmail.com>
2022-03-23 22:27:27 +0000

    Use universal method in comparison for session passing test

--- python/tests/test_session.py
+++ python/tests/test_session.py
@@ -11,7 +11,7 @@ SESSION_FILE = os.path.join(os.path.dirname(__file__), "data", "session.ini")
 @pytest.mark.session
 def test_session_no_ros() -> None:
     with pytest.raises(ros_exc.ProgramNotFoundError):
-        _session = ros_sesh.Session(os.path.dirname(SESSION_FILE))
+        ros_sesh.Session(os.path.dirname(SESSION_FILE))
 
 
 @pytest.mark.session
@@ -24,5 +24,5 @@ def test_session_pass(mocker: pytest_mock.MockerFixture) -> None:
     assert _session.timetable == "Birmingham 0700 Start"
     assert _session.main_mode == ros_enum.Level1Mode(3)
     assert _session.operation_mode == ros_enum.Level2OperMode(3)
-    assert os.path.basename(_session.performance_file) == "Log 23-03-2022 18.46.06; Birmingham; Birmingham 0700 Start.txt"
+    assert _session.performance_file.split("/")[-1] == "Log 23-03-2022 18.46.06; Birmingham; Birmingham 0700 Start.txt"
 
