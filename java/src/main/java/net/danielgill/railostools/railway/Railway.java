92aa3524ef46255570058d6ffc09ea018013563a
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2022-03-23 11:49:28 +0000

    Added timetable library

--- java/src/main/java/net/danielgill/railostools/railway/Railway.java
+++ java/src/main/java/net/danielgill/railostools/railway/Railway.java
@@ -1,5 +1,26 @@
 package net.danielgill.railostools.railway;
 
+import java.util.List;
+
+import net.danielgill.ros.timetable.location.Location;
+import net.danielgill.ros.timetable.location.NamedLocation;
+import net.danielgill.ros.timetable.location.StartLocation;
+
 public class Railway {
+    private List<NamedLocation> namedLocations;
+    private List<Location> locations;
+    private List<StartLocation> startLocations;
+
+    public List<NamedLocation> getNamedLocations() {
+        return namedLocations;
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+    
+    public List<StartLocation> getStartLocations() {
+        return startLocations;
+    }
     
 }
