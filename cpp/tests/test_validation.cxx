7d9f5969d838fa4405ab953c841beed41ced42d9
Kristian Zarębski <krizar312@gmail.com>
2023-01-15 21:05:40 +0000

    Added missing test macro comma

--- cpp/tests/test_validation.cxx
+++ cpp/tests/test_validation.cxx
@@ -2,13 +2,13 @@
 #include <stdexcept>
 #include "railostools/validation/numeric.hxx"
 
-TEST(ValidationTest TestNumericValidationInt) {
+TEST(ValidationTest, TestNumericValidationInt) {
     RailOSTools::NumericValidator<int> validator(10, "test");
     ASSERT_NO_THROW(validator.gt(2).lt(12));
     ASSERT_THROWS(validator.ge(0).le(5), std::runtime_error);
 }
 
-TEST(ValidationTest TestNumericValidationFloat) {
+TEST(ValidationTest, TestNumericValidationFloat) {
     RailOSTools::NumericValidator<float> validator(10.7, "test");
     ASSERT_NO_THROW(validator.gt(2.4).lt(12.1));
     ASSERT_THROWS(validator.ge(0.2).le(5.8), std::runtime_error);
