5fd608857cc522f4d7bcab7a6a167baa009e2f5a
Kristian Zarębski <krizar312@gmail.com>
2023-01-16 17:11:02 +0000

    Move value in validator to validate function

--- cpp/include/railostools/validation/numeric.hxx
+++ cpp/include/railostools/validation/numeric.hxx
@@ -7,29 +7,28 @@ namespace RailOSTools {
     template<typename T>
     class NumericValidator {
         private:
-            const T value_;
             const std::string label_;
             std::optional<T> lt_=std::nullopt;
             std::optional<T> gt_=std::nullopt;
             std::optional<T> ge_=std::nullopt;
             std::optional<T> le_=std::nullopt;
         public:
-            NumericValidator(T value, const std::string& label) : label_{label}, value_{value} {}
+            NumericValidator(const std::string& label) : label_{label} {}
             NumericValidator lt(const T& value) {lt_ = value; return *this;}
             NumericValidator le(const T& value) {le_ = value; return *this;}
             NumericValidator gt(const T& value) {gt_ = value; return *this;}
             NumericValidator ge(const T& value) {ge_ = value; return *this;}
-            std::optional<T> validate() {
-                if(lt_ && lt_.value() <= value_) {
+            std::optional<T> validate(const T& value) {
+                if(lt_ && lt_.value() <= value) {
                     throw std::runtime_error("Validation of '" + label_ + "' failed, value >= " + std::to_string(lt_.value()));
                 }
-                if(le_ && le_.value() < value_) {
+                if(le_ && le_.value() < value) {
                     throw std::runtime_error("Validation of '" + label_ + "' failed, value > " + std::to_string(lt_.value()));
                 }
-                if(gt_ && gt_.value() >= value_) {
+                if(gt_ && gt_.value() >= value) {
                     throw std::runtime_error("Validation of '" + label_ + "' failed, value <= " + std::to_string(lt_.value()));
                 }
-                if(ge_ && ge_.value() > value_) {
+                if(ge_ && ge_.value() > value) {
                     throw std::runtime_error("Validation of '" + label_ + "' failed, value < " + std::to_string(lt_.value()));
                 }
                 return value_;
--- cpp/tests/test_validation.cxx
+++ cpp/tests/test_validation.cxx
@@ -3,13 +3,13 @@
 #include "railostools/validation/numeric.hxx"
 
 TEST(ValidationTest, TestNumericValidationInt) {
-    RailOSTools::NumericValidator<int> validator(10, "test");
-    ASSERT_NO_THROW(validator.gt(2).lt(12));
-    ASSERT_THROW(validator.ge(0).le(5), std::runtime_error);
+    RailOSTools::NumericValidator<int> validator("test");
+    ASSERT_EQ(validator.gt(2).lt(12).validate(10.7), 10.7);
+    ASSERT_THROW(validator.ge(0).le(5).validate(1.7), std::runtime_error);
 }
 
 TEST(ValidationTest, TestNumericValidationFloat) {
-    RailOSTools::NumericValidator<float> validator(10.7, "test");
-    ASSERT_NO_THROW(validator.gt(2.4).lt(12.1));
-    ASSERT_THROW(validator.ge(0.2).le(5.8), std::runtime_error);
+    RailOSTools::NumericValidator<float> validator("test");
+    ASSERT_EQ(validator.gt(2.4).lt(12.1).validate(10.7), 10.7);
+    ASSERT_THROW(validator.ge(0.2).le(5.8).validate(10.7), std::runtime_error);
 }
