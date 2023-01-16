b4010a655d6e777bff81c33d4b670f05609cf7c3
Kristian Zarębski <krizar312@gmail.com>
2023-01-16 17:02:24 +0000

    Fix bad test macro

--- cpp/tests/test_validation.cxx
+++ cpp/tests/test_validation.cxx
@@ -5,11 +5,11 @@
 TEST(ValidationTest, TestNumericValidationInt) {
     RailOSTools::NumericValidator<int> validator(10, "test");
     ASSERT_NO_THROW(validator.gt(2).lt(12));
-    ASSERT_THROWS(validator.ge(0).le(5), std::runtime_error);
+    ASSERT_THROW(validator.ge(0).le(5), std::runtime_error);
 }
 
 TEST(ValidationTest, TestNumericValidationFloat) {
     RailOSTools::NumericValidator<float> validator(10.7, "test");
     ASSERT_NO_THROW(validator.gt(2.4).lt(12.1));
-    ASSERT_THROWS(validator.ge(0.2).le(5.8), std::runtime_error);
+    ASSERT_THROW(validator.ge(0.2).le(5.8), std::runtime_error);
 }
